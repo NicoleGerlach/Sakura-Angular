23ea9e30ea853899198b8e8eb1b47eab4dd4d93a
Nicole Gerlach <coleni82@gmail.com>
2025-06-02 17:03:50 +0200

    first commit

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
